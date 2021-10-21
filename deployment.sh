01d6e6e0b8c1d3e4a083dc5ac67177d243077f9a
Ritesh <riteshsahu1981@gmail.com>
2021-10-21 20:27:29 +0530

    permission issue fixed

--- deployment.sh
+++ deployment.sh
@@ -6,6 +6,7 @@ chmod -R 755 /home/ubuntu/laravelpoc_build/
 rsync -avzh /home/ubuntu/laravelpoc_build/ /var/www/html/laravelpoc/
 
 chmod -R 755 /var/www/html/laravelpoc/
+chmod -R 777 /var/www/html/laravelpoc/storage/
 cd /var/www/html/laravelpoc/
 php artisan migrate
 echo "Deployment Done.."
