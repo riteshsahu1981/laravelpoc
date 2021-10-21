85f46603cda5c4c1494154892c59070d1dca2843
Ritesh <riteshsahu1981@gmail.com>
2021-10-21 20:19:23 +0530

    path change

--- deployment.sh
+++ deployment.sh
@@ -2,8 +2,8 @@
 cd /var/www/html/laravelpoc_build/
 git stash
 git pull --no-edit
-chmod -R 755 /var/www/html/laravelpoc_build/
-rsync -avzh /var/www/html/laravelpoc_build/ /var/www/html/laravelpoc/
+chmod -R 755 /home/ubuntu/laravelpoc_build/
+rsync -avzh /home/ubuntu/laravelpoc_build/ /var/www/html/laravelpoc/
 
 chmod -R 755 /var/www/html/laravelpoc/
 cd /var/www/html/laravelpoc/
